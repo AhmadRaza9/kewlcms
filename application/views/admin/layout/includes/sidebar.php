c698e31d3cbc44d67a7a3e5e3e9f118e68f0fd52
Ahmad Raza <raza.ataki@gmail.com>
2022-01-19 20:56:42 +0500

    update sidebar links active

--- application/views/admin/layout/includes/sidebar.php
+++ application/views/admin/layout/includes/sidebar.php
@@ -5,31 +5,31 @@
   <div class="position-sticky pt-3">
     <ul class="nav flex-column">
       <li class="nav-item">
-        <a class="nav-link active" href="http://kewlcms.test/index.php/admin/dashboard">
-          <i class="bi bi-house-fill"></i> Dashboard
+        <a class="nav-link <?php if ($this->uri->segment(2) === 'dashboard') {echo "active";}?>" href="http://kewlcms.test/index.php/admin/dashboard">
+          <i class="bi bi-house-fill"></i> Dashboard<br>
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="http://kewlcms.test/index.php/admin/articles/index">
+        <a class="nav-link <?php if ($this->uri->segment(2) === 'articles') {echo "active";}?>" href="http://kewlcms.test/index.php/admin/articles/index">
           <span data-feather="file"></span>
           <i class="bi bi-wallet-fill"></i> Articles
         </a>
       </li>
       <li class="nav-item">
-          <a class="nav-link" href="http://kewlcms.test/index.php/admin/categories">
+          <a class="nav-link <?php if ($this->uri->segment(2) === 'categories') {echo "active";}?>" href="http://kewlcms.test/index.php/admin/categories">
           <span data-feather="shopping-cart"></span>
           <i class="bi bi-tags-fill"></i> Categories
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="http://kewlcms.test/index.php/admin/users">
+        <a class="nav-link <?php if ($this->uri->segment(2) === 'users') {echo "active";}?>" href="http://kewlcms.test/index.php/admin/users">
           <span data-feather="users"></span>
           <i class="bi bi-file-earmark-person-fill"></i> Users
         </a>
       </li>
 
       <li class="nav-item">
-        <a class="nav-link" href="http://kewlcms.test/index.php/admin/settings">
+        <a class="nav-link <?php if ($this->uri->segment(2) === 'settings') {echo "active";}?>" href="http://kewlcms.test/index.php/admin/settings">
           <span data-feather="layers"></span>
           <i class="bi bi-gear-fill"></i> Settings
         </a>
